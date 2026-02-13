75e97cec313f3d2b8e1e081993ca159883a4c15a
Andrei Alergush <andrey392live@gmail.com>
2026-02-13 13:31:22 +0200

    Setup: Configured Firebase, Firestore, and CORS in Program.cs

--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -1,8 +1,57 @@
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Firestore;
+using Google.Cloud.Firestore.V1;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+string path = Path.Combine(AppContext.BaseDirectory, "firebase_key.json");
+
+if (!File.Exists(path))
+{
+    throw new Exception("firebase_key.json not found");
+}
+
+var firebaseProjectId = builder.Configuration["Firebase:ProjectId"];
+
+if (string.IsNullOrWhiteSpace(firebaseProjectId))
+{
+    throw new Exception("Firebase ProjectId not found in appsettings.json");
+}
+
+GoogleCredential credential;
+
+using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+{
+    var serviceAccount = ServiceAccountCredential.FromServiceAccountData(stream);
+    credential = GoogleCredential.FromServiceAccountCredential(serviceAccount);
+}
+
+FirebaseApp.Create(new AppOptions() { Credential = credential });
+
+builder.Services.AddSingleton(sp =>
+{
+    var clientBuilder = new FirestoreClientBuilder { Credential = credential };
+    var client = clientBuilder.Build();
+
+    return FirestoreDb.Create(firebaseProjectId, client);
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        }
+    );
+});
 
 var app = builder.Build();
 
@@ -12,30 +61,10 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseCors("AllowAll");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
